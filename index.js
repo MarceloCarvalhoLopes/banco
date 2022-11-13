7db69b8da3d87fcd1ae4f1760a88cd7e2feda0bd
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-11-13 10:34:25 -0300

    insert

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 var database = require("./database");
 
+var dados = {
+    name: "Minecraft",
+    price: 37.55
+}
+
+/*var query  = database.insert(dados).into("games");
+console.log(query.toQuery());*/
+
+database.insert(dados).into("games").then(data =>{
+    console.log(data)
+}).catch(err =>{
+    console.log(err);
+})
