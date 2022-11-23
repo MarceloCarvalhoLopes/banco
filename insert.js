dcc5203aa22166924e88ef9ee87bec3bf8b1f7ca
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-11-23 06:21:06 -0300

    associated inserts

--- insert.js
+++ insert.js
@@ -8,10 +8,18 @@ var dados = {
 /*var query  = database.insert(dados).into("games");
 console.log(query.toQuery());*/
 
-database.insert(dados).into("games").then(data =>{
+/*database.insert(dados).into("games").then(data =>{
     console.log(data)
 }).catch(err =>{
     console.log(err);
+})*/
+
+//Associated inserts
+database.insert({
+    name: "Blizzard",
+    game_id :1
+}).table("estudios").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
 })
-
-
