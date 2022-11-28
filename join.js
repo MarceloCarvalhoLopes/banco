1038eddd24a77e87470ce974c7a51e53e90bf0c9
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-11-28 06:05:44 -0300

    join with where

--- join.js
+++ join.js
@@ -3,7 +3,8 @@ var database = require("./database");
 database.select([" games.id as games_id",
                   "games.name as games_name",                 
                   "estudios.id as est_id",               
-                  "estudios.name as estu_name"]).table("games").innerJoin("estudios","estudios.game_id","games.id")
+                  "estudios.name as estu_name"]).table("games").leftJoin("estudios","estudios.game_id","games.id")
+                  .where("games.id",5)
                   .then(data =>{
                         console.log(data);
                   }).catch(err => {
