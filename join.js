cb214d723ce2f3f3547e2fb3491832b9d937259b
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-11-28 06:25:40 -0300

    one to N

--- join.js
+++ join.js
@@ -10,3 +10,28 @@ database.select([" games.id as games_id",
                   }).catch(err => {
                         console.log(err);
                   })
+
+
+database.select([" games.id as games_id",
+                  "games.name as games_name",                 
+                  "estudios.id as est_id",               
+                  "estudios.name as estu_name"]).table("games").innerJoin("estudios","estudios.game_id","games.id")
+                  .then(data =>{
+
+                        var estudiosGameArray = data;
+                        var game = {
+                              id: 0,
+                              name: "",
+                              estudios:[]
+                        }
+
+                        game.id = data[0].games_id;
+                        game.name = data[0].games_name;
+
+                        data.forEach(estudio => {
+                             game.estudios.push({name: estudio.estu_name}) ;
+                        });
+                        console.log(game);
+                  }).catch(err => {
+                        console.log(err);
+                  })                  
