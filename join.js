94f5bc1f85c7aa4f2e8ea1a9f80b14d2174a4340
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-11-29 20:38:52 -0300

    N to N

--- join.js
+++ join.js
@@ -1,37 +1,14 @@
 var database = require("./database");
 
-database.select([" games.id as games_id",
-                  "games.name as games_name",                 
-                  "estudios.id as est_id",               
-                  "estudios.name as estu_name"]).table("games").leftJoin("estudios","estudios.game_id","games.id")
-                  .where("games.id",5)
-                  .then(data =>{
-                        console.log(data);
-                  }).catch(err => {
-                        console.log(err);
-                  })
-
-
-database.select([" games.id as games_id",
-                  "games.name as games_name",                 
-                  "estudios.id as est_id",               
-                  "estudios.name as estu_name"]).table("games").innerJoin("estudios","estudios.game_id","games.id")
-                  .then(data =>{
-
-                        var estudiosGameArray = data;
-                        var game = {
-                              id: 0,
-                              name: "",
-                              estudios:[]
-                        }
-
-                        game.id = data[0].games_id;
-                        game.name = data[0].games_name;
-
-                        data.forEach(estudio => {
-                             game.estudios.push({name: estudio.estu_name}) ;
-                        });
-                        console.log(game);
-                  }).catch(err => {
-                        console.log(err);
-                  })                  
+database.select([
+            "estudios.name as estudios_name",
+            "games.name as games_name"
+      ]).table("games_estudios")
+      .innerJoin("games", "games.id","games_estudios.game_id")
+      .innerJoin("estudios", "estudios.id","games_estudios.estudio_id")
+            .where("games.id",6)
+            .then(data =>{
+                  console.log(data);
+            }).catch(err => {
+                  console.log(err);
+            })                  
